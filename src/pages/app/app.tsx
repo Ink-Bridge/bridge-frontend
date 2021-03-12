1e7534e114b645658a7a74068392ffd702d36fdb
freepoix <17864213831@163.com>
2021-03-12 12:57:08 +0800

    up

--- src/pages/app/app.tsx
+++ src/pages/app/app.tsx
@@ -1,7 +1,7 @@
-import React, { FC, ReactElement, useMemo, useRef } from 'react';
+import React, { FC, ReactElement, useMemo, useRef, useState } from 'react';
 import './app.css';
 import { BlockInfo } from './block-info';
-import { Box, SvgIcon } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 import { useCheckedHeightAndTxs as useData } from '../../core';
 import { useApi } from '../../core/hooks/useApi';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
@@ -9,19 +9,63 @@ import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 const Main: FC = (): ReactElement => {
   const { checkedHeight, txs, blockList } = useData();
-  // const [ prevAvail]
+  const [ prevAvailable, setPrevAvailable ] = useState<boolean>(false);
+  const [ nextAvailable, setNextAvailable ] = useState<boolean>(true);
   const filteredBlockList = useMemo(() => blockList.slice(-20), [blockList]);
   const content = useRef<HTMLDivElement>(null);
+  const container = useRef<HTMLDivElement>(null);
   const handleNext = () => {
-    if (!content.current) {
+    if (!content.current || !container.current) {
       return;
     }
-    const left = parseFloat(content.current?.style.left || '0');
-    content.current && (content.current.style.left = `${left - 300}px`);
+    let left = parseFloat(content.current?.style.left || '0');
+    const min = container.current.clientWidth - content.current.clientWidth;
+    if (left - 200 < min) {
+      left = min;
+      setNextAvailable(false);
+    } else {
+      left -= 200;
+    }
+    console.log('min', min, 'left', left);
+        
+    if (left === 0) {
+      setPrevAvailable(false);
+    } else {
+      setPrevAvailable(true);
+    }
+    if (left === min) {
+      setNextAvailable(false);
+    } else {
+      setNextAvailable(true);
+    }
+
+    content.current && (content.current.style.left = `${left}px`);
   };
   const handlePrev = () => {
-    const left = parseFloat(content.current?.style.left || '0');
-    content.current && (content.current.style.left = `${left + 300}px`);
+    if (!content.current || !container.current) {
+      return;
+    }
+    const min = container.current.clientWidth - content.current.clientWidth;
+    let left = parseFloat(content.current?.style.left || '0');
+    if (left + 200 > 0) {
+      left = 0;
+    } else {
+      left += 200;
+    }
+    console.log('min', min, 'left', left);
+    
+    if (left === 0) {
+      setPrevAvailable(false);
+    } else {
+      setPrevAvailable(true);
+    }
+    if (left === min) {
+      setNextAvailable(false);
+    } else {
+      setNextAvailable(true);
+    }
+
+    content.current && (content.current.style.left = `${left}px`);
   };
 
   return (
@@ -29,7 +73,7 @@ const Main: FC = (): ReactElement => {
       <Box>
         <h1 style={{ textAlign: 'left', lineHeight: '8rem', fontSize: '4rem', marginLeft: '4.2rem', height: '8rem', color: '#111', fontWeight: 600 }}>InkBridge</h1>
       </Box>
-      <Box width="100%" overflow="hidden" position="absolute" top="8rem" bottom="10rem">
+      <div ref={container} style={{ width: "100%", overflow: "hidden", position: "absolute", top: "8rem", bottom: "10rem" }}>
         <div ref={content} style={{ height: "100%", padding: "4.8rem", position: "absolute", left: "0px", top: "0px" }}>
           <Box zIndex="1" height="40px" padding="18px 0px">
             <Box borderTop="4px dashed #E7D5C0"></Box>
@@ -39,14 +83,14 @@ const Main: FC = (): ReactElement => {
             { filteredBlockList.slice(1).map(block => <BlockInfo checked={checkedHeight >= block.height} key={block.hash} blockData={block} txs={txs[block.height] || []}></BlockInfo>) }
           </Box>
         </div>
-      </Box>
-      <Box bgcolor="rgb(239, 239, 239)" height="10rem" width="100%" justifyContent="space-between" display="flex" position="absolute" bottom="0px">
-        <Box style={{ height: '10rem', width: '8rem', background: '#F7931A', color: 'white', display: 'flex', flexDirection: 'column', justifyContent: 'space-around'}}>
+      </div>
+      <Box height="10rem" width="100%" justifyContent="space-between" display="flex" position="absolute" bottom="0px">
+        <Box style={{ height: '10rem', width: '8rem', background: prevAvailable ? '#F7931A' : '#D2D2D2', color: prevAvailable ? 'white' : 'rgb(232, 232, 232)', display: 'flex', flexDirection: 'column', justifyContent: 'space-around'}}>
           <Box style={{ cursor: 'pointer', display: 'flex', justifyContent: 'center', width: '100%' }} onClick={handlePrev}>
             <ArrowBackIosIcon style={{ fontSize: '4rem' }}/>
           </Box>
         </Box>
-        <Box style={{ height: '10rem', width: '8rem', background: '#F7931A', color: 'white', display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}>
+        <Box style={{ height: '10rem', width: '8rem', background: nextAvailable ? '#F7931A' : '#D2D2D2', color: nextAvailable ? 'white' : 'rgb(232, 232, 232)', display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}>
           <Box style={{ cursor: 'pointer', display: 'flex', justifyContent: 'center', width: '100%' }} onClick={handleNext}>
             <ArrowForwardIosIcon style={{ fontSize: '4rem' }}/>
           </Box>
--- src/pages/app/block-info.tsx
+++ src/pages/app/block-info.tsx
@@ -2,6 +2,7 @@ import { Box } from '@material-ui/core';
 import React, { FC, ReactElement } from 'react';
 import { Block, Tx } from '../../core';
 import { TxInfo } from './tx-info';
+import './block-info.css';
 
 interface IProps {
   blockData: Block;
@@ -14,20 +15,23 @@ export const BlockInfo: FC<IProps> = ({
   txs,
   checked = false,
 }): ReactElement => {
+
   return (
-    <Box>
-      <Box width="37rem" height="20rem" bgcolor="white" marginRight="2.2rem" boxShadow='0px 0px 24px 0px rgba(0,0,0,0.08)'>
+    <div className="block-info">
+      <Box width="37rem" height="20rem" bgcolor="white" boxShadow='0px 0px 24px 0px rgba(0,0,0,0.08)'>
         <Box color="white" textAlign="left" height="5rem" lineHeight="5rem" paddingLeft="2.2rem" bgcolor="#E5AA64" position="relative">
           <span>
             <span style={{ fontWeight: 700 }}>Relayer:&nbsp;&nbsp;</span>
             { blockData.relayer.slice(0, 7) }
           </span>
-          <Box textAlign="center" width="10rem" height="10rem" bgcolor="#F7931A" position="absolute" bottom="0px" right="0px">
-            <div style={{ fontSize: '2.4rem', position: 'relative', top: '1rem' }}>
-              { blockData.height }
+          <a href={`https://www.blockchain.com/btc/block/${blockData.hash.slice(2)}`} target="blank">
+            <div className="block-number">
+              <div style={{ fontSize: '2.4rem', position: 'relative', top: '1rem' }}>
+                { blockData.height }
+              </div>
+              <span>block</span>
             </div>
-            <span>block</span>
-          </Box>
+          </a>
         </Box>
         
         <Box>
@@ -47,12 +51,12 @@ export const BlockInfo: FC<IProps> = ({
       </Box>
       {
         !!txs.length &&
-          <Box width="37rem" marginRight="2.2rem" marginTop="1rem" bgcolor="white" padding="1.8rem" boxShadow='0px 0px 24px 0px rgba(0,0,0,0.08)'>
+          <Box width="37rem" marginTop="1rem" bgcolor="white" padding="1.8rem" boxShadow='0px 0px 24px 0px rgba(0,0,0,0.08)'>
             {
               txs.map(tx => <TxInfo key={tx.tx_hash} tx={tx}></TxInfo>)
             }
           </Box>
       }
-    </Box>
+    </div>
   );
 };
