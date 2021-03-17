d458591d8e8c1f9a5f8c7b98444d09d38230c895
freepoix <17864213831@163.com>
2021-03-17 11:29:45 +0800

    up

--- src/core/contracts/contract-address.ts
+++ src/core/contracts/contract-address.ts
@@ -1,2 +1,2 @@
-export const ApplicationAddr = '3eFZNgegCNw7XMVfQcgzRVyFjvveGejqUQZPy7rSWLWDHAKF';
-export const BridgeAddr = '3dbw5t2MwQetceBCu9Lz7SKGZ5aBTogwChU33b9qm22nSQN8';
+export const ApplicationAddr = '3c8xSLkeQJ6NiLWze2Yfpm9bLjSy4zsTDaMmR1Fe5ycqxG39';
+export const BridgeAddr = '3f9ahX4WzcZnTxzZs9R7njKPMoMGN2jp4cVpFeG2SSEi9HeR';
