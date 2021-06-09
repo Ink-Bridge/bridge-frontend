16827903e3527b74e157a9346c5775b6a16a1a98
Zenacia <17864213831@163.com>
2021-06-09 18:30:56 +0800

    update endpoints & contracts address

--- src/core/contracts/contract-address.ts
+++ src/core/contracts/contract-address.ts
@@ -1,2 +1,2 @@
-export const ExampleAddr = '3gYQ9b2a4KQkSMBAKerAFZAJNeqKFTZ67NJw3URPDMTSUZVt';
-export const BridgeAddr = '3fDJD72VYyp3aaChD5hmszBeXRgyverZscFtuzhZuKdTfGZo';
+export const ExampleAddr = '3eEz7yWLoj9oDNTrjasAAhwKyt3koFPvsCd34AveLLnUe5V7';
+export const BridgeAddr = '3c7X3j2JsNRCU5RqwMMdXb571s7AuyTfy1gtCt758KQkX64L';
